c05ba3574f679e16317aee2fc5641cee3e4bff0b
Henry Thach <thachhao@gmail.com>
2016-05-01 08:53:24 -0400

    Add wiredep and inject.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
-var gulp = require('gulp'),
-    eslint = require('gulp-eslint');
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var inject = require('gulp-inject');
 
 gulp.task('lint', function () {
-  gulp.src(['public/DND/app/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    return gulp
+        .src(['public/DND/app/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint']);
+gulp.task('wiredep', function () {
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/DND/bower_components',
+        ignorePath: './public'
+    };
+    var wiredep = require('wiredep').stream;
+    return gulp
+        .src('./public/DND/index.html')
+        .pipe(wiredep(options))
+        .pipe(inject(gulp.src([
+            './public/DND/app/**/*.js',
+            '!./public/DND/bower_components/**/*.js',
+            './public/DND/css/**/*.css'
+        ]), { relative: true }))
+        .pipe(gulp.dest('./public/DND/'))
+});
+
+gulp.task('default', ['lint', 'wiredep']);
