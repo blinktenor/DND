24fdfb58476873216031276c4c1344d24ac0aa30
Bob Moore <blinktenor@gmail.com>
2016-06-14 22:23:17 -0400

    Code cleanup and setting default port in settings.

--- app.js
+++ app.js
@@ -13,22 +13,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 app.engine('html', require('ejs').renderFile);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
-//app.use(multer({ dest: './uploads/'}));
 
 app.use('/', routes);
 
 //fixes index.html
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', routes);
-//app.use('/DND/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,8 +33,7 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
+// 
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
